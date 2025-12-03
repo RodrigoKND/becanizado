bf3f3b87286d60325167fde912adc5c0e8d15b27
rodriknd <rp752989@gmail.com>
2025-12-02 21:53:09 -0400

    Enhance SelectMatter component styling for better visibility

--- src/components/SelectMatter.tsx
+++ src/components/SelectMatter.tsx
@@ -2,7 +2,7 @@ import { MATTERS } from "../data/matters";
 
 export default function SelectMatter() {
     return (
-        <select className="w-full p-4 py-2 cursor-pointer rounded-md" >
+        <select className="w-full p-4 py-2 cursor-pointer rounded-md text-gray-800" >
             {
                 MATTERS.map((matter) => (
                     <option key={matter} value={matter}>
